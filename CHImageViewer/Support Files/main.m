3e02166596e8609c7aeb7c9cc9aaba3b3bf30403
Chiho <chiho@youthmba.com>
2016-12-24 14:09:39 +0800

    初始化
    初始化

---
+++ CHImageViewer/Support Files/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  CHImageViewer
+//
+//  Created by ChiHo on 2016/12/24.
+//  Copyright © 2016年 Chiho. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
